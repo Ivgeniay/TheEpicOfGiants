b8b1de8a747e326310813d8598db1d2ca298fdd6
Engeniy S <motleycrue6502@gmail.com>
2023-12-12 15:50:07 +0400

    upd: titan hand trigger and select

---
+++ Assets/_Project/CodeBase/XR/HandsAnimations/HandsAnimationController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace CodeBase.XR.HandsAnimations
+{
+    internal class HandsAnimationController : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        [SerializeField] private InputActionProperty triggerAnimationAction;
+        [SerializeField] private InputActionProperty gripAnimationAction;
+
+        public static readonly int Grip = Animator.StringToHash("Grip"); 
+        public static readonly int Trigger = Animator.StringToHash("Trigger");
+
+        private void Update()
+        {
+            animator.SetFloat(Grip, gripAnimationAction.action.ReadValue<float>());
+            animator.SetFloat(Trigger, triggerAnimationAction.action.ReadValue<float>()); 
+        }
+
+    }
+}
